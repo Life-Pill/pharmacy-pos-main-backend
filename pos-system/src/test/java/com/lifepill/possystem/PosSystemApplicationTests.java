bee8695cba7fc5d280210b2e9c87f633941ee48b
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-11-30 08:25:58 +0530

    conect aws postgresql database

---
+++ pos-system/src/test/java/com/lifepill/possystem/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifepill.possystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
