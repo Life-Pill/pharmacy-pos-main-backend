3115d4b624c3fd10758dd772b6246d3767eab467
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-12-02 03:17:40 +0530

    swagger config

--- pos-system/src/main/java/com/lifepill/possystem/entity/Cashier.java
+++ pos-system/src/main/java/com/lifepill/possystem/entity/Cashier.java
@@ -1,10 +1,12 @@
 package com.lifepill.possystem.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
