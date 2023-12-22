feef9eb1647466d9eb90af85747cdefb971fdf6c
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-12-22 18:50:16 +0530

    [Fix] Resolve Pagination Issue by Correcting Pageable Import Statement

--- pos-system/src/main/java/com/lifepill/possystem/repo/ItemRepo.java
+++ pos-system/src/main/java/com/lifepill/possystem/repo/ItemRepo.java
@@ -2,11 +2,13 @@ package com.lifepill.possystem.repo;
 
 import com.lifepill.possystem.entity.Item;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
+//import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
