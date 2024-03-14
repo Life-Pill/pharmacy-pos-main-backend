849b82b3a0f8d37ba67a65002d5ed8b8cca2ccbb
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-14 17:13:07 +0530

    add data SQL Queries

---
+++ pos-system/SQL_Queries/cashierbankdetails.sql
@@ -0,0 +1,38 @@
+INSERT INTO cashierbankdetails (
+    bankAccountNumber,
+    bankBranchName,
+    bankName,
+    cashierDescription,
+    cashierId,
+    monthlyPayment,
+    monthlyPaymentStatus
+) VALUES
+-- Cashier 101
+('123456789', 'Main Branch', 'ABC Bank', 'Savings Account', 101, 3000, true),
+
+-- Cashier 102
+('987654321', 'Oak Branch', 'XYZ Bank', 'Checking Account', 102, 2800, true),
+
+-- Cashier 103
+('543210987', 'Pine Branch', 'DEF Bank', 'Savings Account', 103, 3200, true),
+
+-- Cashier 104
+('456789012', 'Elm Branch', 'GHI Bank', 'Checking Account', 104, 3100, true),
+
+-- Cashier 105
+('654321098', 'Cedar Branch', 'JKL Bank', 'Savings Account', 105, 3300, true),
+
+-- Cashier 106
+('789012345', 'Pine Branch', 'MNO Bank', 'Checking Account', 106, 2900, true),
+
+-- Cashier 107
+('210987654', 'Maple Branch', 'PQR Bank', 'Savings Account', 107, 3000, true),
+
+-- Cashier 108
+('543210987', 'Birch Branch', 'STU Bank', 'Checking Account', 108, 2700, true),
+
+-- Cashier 109
+('678901234', 'Oak Branch', 'VWX Bank', 'Savings Account', 109, 3200, true),
+
+-- Cashier 110
+('432109876', 'Cedar Branch', 'YZA Bank', 'Checking Account', 110, 2800, true);
---
+++ pos-system/SQL_Queries/order.sql
@@ -0,0 +1,13 @@
+{
+  "cashiers": 102,
+  "orderDate": "2024-03-14T11:33:55.681Z",
+  "orderDetails": [
+    {
+      "itemName": "NoseClear",
+      "items": 10,
+      "qty": 2,
+      "totalAmount": 160
+    }
+  ],
+  "total": 160
+}
