dadc9fabf67864e308d2de1a39a1ba5f31f29e1a
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-03 08:53:42 +0530

    update cors policy

--- pos-system/src/main/java/com/lifepill/possystem/config/WebMvcConfig.java
+++ pos-system/src/main/java/com/lifepill/possystem/config/WebMvcConfig.java
@@ -22,6 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
