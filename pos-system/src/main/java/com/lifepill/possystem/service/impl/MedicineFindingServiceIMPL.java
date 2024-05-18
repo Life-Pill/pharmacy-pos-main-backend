2ce94e362ae0b97027f841efd2168a1c3d0fd988
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-19 01:29:11 +0530

    get medicine by name

--- pos-system/src/main/java/com/lifepill/possystem/service/impl/MedicineFindingServiceIMPL.java
+++ pos-system/src/main/java/com/lifepill/possystem/service/impl/MedicineFindingServiceIMPL.java
@@ -2,44 +2,39 @@ package com.lifepill.possystem.service.impl;
 
 import com.lifepill.possystem.dto.BranchDTO;
 import com.lifepill.possystem.dto.ItemCategoryDTO;
-import com.lifepill.possystem.dto.responseDTO.ItemGetResponseDTO;
 import com.lifepill.possystem.dto.responseDTO.MedicineGetResponseDTO;
 import com.lifepill.possystem.entity.Branch;
 import com.lifepill.possystem.entity.Item;
 import com.lifepill.possystem.exception.NotFoundException;
 import com.lifepill.possystem.repo.branchRepository.BranchRepository;
-import com.lifepill.possystem.repo.itemRepository.ItemCategoryRepository;
 import com.lifepill.possystem.repo.itemRepository.ItemRepository;
-import com.lifepill.possystem.repo.supplierRepository.SupplierRepository;
 import com.lifepill.possystem.service.MedicineFindingService;
-import com.lifepill.possystem.util.mappers.ItemMapper;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implementation of the MedicineFindingService interface.
+ * Provides methods to find items (medicines) by name.
+ */
 @Service
 @AllArgsConstructor
 public class MedicineFindingServiceIMPL implements MedicineFindingService {
 
-
     private ItemRepository itemRepository;
-
-
     private BranchRepository branchRepository;
-
     private ModelMapper modelMapper;
 
-    private ItemMapper itemMapper;
-
-    private ItemCategoryRepository itemCategoryRepository;
-
-    private SupplierRepository supplierRepository;
-
+    /**
+     * Retrieves a list of MedicineGetResponseDTO objects representing items with the given name.
+     *
+     * @param itemName the name of the item to search for
+     * @return a list of MedicineGetResponseDTO objects representing the found items
+     * @throws NotFoundException if no items are found with the given name
+     */
     @Override
     public List<MedicineGetResponseDTO> getItemByName(String itemName) {
         List<Item> items = itemRepository.findAllByItemName(itemName);
@@ -58,10 +53,8 @@ public class MedicineFindingServiceIMPL implements MedicineFindingService {
             if (branch != null) {
                 medicineGetResponseDTO.setBranchDTO(modelMapper.map(branch, BranchDTO.class));
             }
-
             medicineGetResponseDTOS.add(medicineGetResponseDTO);
         }
-
         return medicineGetResponseDTOS;
     }
 }
