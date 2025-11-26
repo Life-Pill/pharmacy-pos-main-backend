33c2c7bcf891f44b881ce8873878198ca838e297
Pramitha Jayasooriya <lpramithamj@gmail.com>
2025-11-27 02:57:52 +0530

    Add Hibernate and Redis configurations, update Swagger API documentation, and fix versioning in pom.xml

--- pos-system/src/main/java/com/lifepill/possystem/config/SwaggerConfig.java
+++ pos-system/src/main/java/com/lifepill/possystem/config/SwaggerConfig.java
@@ -1,21 +1,44 @@
-/*
 package com.lifepill.possystem.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * Swagger/OpenAPI configuration with JWT Bearer token authentication.
+ */
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
     @Bean
-    public Docket SwaggerApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("LifePill POS System API")
+                        .description("REST API documentation for LifePill Pharmacy POS System")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("LifePill Team")
+                                .email("support@lifepill.com")
+                                .url("https://lifepill.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token. Get token from /lifepill/v1/auth/authenticate endpoint")));
     }
 }
-*/
